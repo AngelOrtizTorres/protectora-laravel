275a7c31eca3528acd8ee2af2bdb09bb587fbe42
Angel Ortiz <4ngel0rtiz7orres0330@gmail.com>
2026-01-19 16:08:29 +0100

    Add app-layouts.blade.php for base layout structure using Tailwind CSS

---
+++ resources/views/components/app-layouts.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Protectora'}}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="{{ $class }}">
+    
+    {{ $slot }}
+
+</body>
+</html>
