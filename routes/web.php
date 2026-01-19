81bb540e00f32d9f298e480249d932fe67d7a1f0
Angel Ortiz <4ngel0rtiz7orres0330@gmail.com>
2026-01-19 16:08:22 +0100

    Refactor routes in web.php to add named routes for better clarity and organization

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,22 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', IndexController::class);
+Route::get('/', IndexController::class)->name('root');
 
-Route::get('/usuarios', UserController::class)->name('usuarios');
-Route::get('registro/usuarios', [AuthController::class, 'showUserRegisterForm']);
+Route::get('/usuarios', [UserController::class, 'show'])->name('users');
+Route::get('registro/usuarios', [AuthController::class, 'showUserRegisterForm'])->name('show.register.users');
 Route::post('registro/usuarios', [AuthController::class, 'registerUser'])->name('register.user');
 
-Route::get('/animales', AnimalController::class)->name('animales');
-Route::get('registro/animales', [AuthController::class, 'showAnimalRegisterForm']);
+Route::get('/animales', [AnimalController::class, 'show'])->name('animals');
+Route::get('registro/animales', [AuthController::class, 'showAnimalRegisterForm'])->name('show.register.animal');
 Route::post('registro/animales', [AuthController::class, 'registerAnimal'])->name('register.animal');
 
-Route::get('/login', [AuthController::class, 'showLoginForm']);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+Route::get('/usuarios/editar/{user}', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/usuarios/{user}', [UserController::class, 'update'])->name('user.update');
+Route::delete('/usuarios/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+
+Route::get('/animales/editar/{animal}', [AnimalController::class, 'edit'])->name('animal.edit');
+Route::put('/animales/{animal}', [AnimalController::class, 'update'])->name('animal.update');
+Route::delete('/animales/{animal}', [AnimalController::class, 'destroy'])->name('animal.destroy');
